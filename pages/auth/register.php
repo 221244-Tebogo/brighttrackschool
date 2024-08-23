4982e13eec0a169d3f859700e40da6bc407f1533
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2024-08-23 03:01:04 +0200

    working registration code

--- pages/auth/register.php
+++ pages/auth/register.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
-    $conn->close(); // Close the database connection
+    $conn->close();
 }
 ?>
 
